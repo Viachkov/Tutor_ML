e8403bd0c2c36a31a10a0840f30c7c31ffcc651c
vyachkov <viachkov@hitmail.com>
2020-10-22 22:34:15 +0300

    Start error check

--- lesson_02.py
+++ lesson_02.py
@@ -1,5 +1,13 @@
 def main():
-    pass
+
+    knob_weight = 0.5
+    income = 0.5
+    goal_pred = 0.8
+
+    pred = income * knob_weight
+    error = (pred - goal_pred)**2
+    print(error)
+    
 
 
 if __name__ == "__main__":

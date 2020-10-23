64d6701a4836ca728e1f90c6c438aec177eb5a00
vyachkov <viachkov@hitmail.com>
2020-10-23 10:44:05 +0300

    Lesson_2 errors

--- lesson_02.py
+++ lesson_02.py
@@ -1,13 +1,33 @@
 def main():
-
-    knob_weight = 0.5
-    income = 0.5
-    goal_pred = 0.8
-
-    pred = income * knob_weight
-    error = (pred - goal_pred)**2
-    print(error)
+    weight = 0.1
+    lr = 0.01
+    def neural_nerwork(income, weight):
+        pred = income * weight
+        return pred
     
+    number_of_toes = [0.5]
+    win_lose_binary = [1]
+    income = number_of_toes[0]
+    true = win_lose_binary[0]
+
+    prediction = neural_nerwork(income, weight)
+    error = (prediction - true)**2
+    print(error)
+
+    prediction_up = neural_nerwork(income, weight) + lr
+    error_up = (true - prediction_up)**2
+
+    prediction_down = neural_nerwork(income, weight) - lr
+    error_down = (true - prediction_down)**2
+
+    if error > error_down or error > error_up:
+        if error_down > error_up:
+            weight += lr
+            print('Error_up', error_up)
+        else:
+            weight += lr
+            print('Error_down', error_down)
+
 
 
 if __name__ == "__main__":

b98db123646ee04eb8cb2a76d6fe91f592acc916
vyachkov <viachkov@hitmail.com>
2020-10-24 17:13:42 +0300

    Gradient decent

--- lesson_02.py
+++ lesson_02.py
@@ -1,28 +1,21 @@
 def main():
     weight = 0.5
-    income = 0.5
-    goal_prediction = 0.8
+    alpha = 0.01    # speed of learning
 
-    step_amount = 0.001
+    def neural_network(income, weight):
+        pred = income * weight
+        return pred
 
-    for iteration in range(1101):
-        prediction = income * weight
-        error = (goal_prediction - prediction)**2
+    number_of_toes = [0.5]
+    win_or_lose_binary = [1]
+    income = number_of_toes[0]
+    goal_pred = win_or_lose_binary[0]
 
-        print('error:' + str(error) + ' | Prediction: ' + str(prediction))
-
-        prediction_up = income * (weight + step_amount)
-        error_up = (goal_prediction - prediction_up)**2
-
-        prediction_down = income * (weight - step_amount)
-        error_down = (goal_prediction - prediction_down)**2
-
-        if error_down < error_up:
-            weight -= step_amount
-        
-        if error_up < error_down:
-            weight += step_amount
-    
+    prediction = neural_network(income, weight)
+    error = (prediction - goal_pred)**2
+    delta = prediction - goal_pred
+    weight_delta = delta * income
+    weight -= weight_delta * alpha
 
 
 

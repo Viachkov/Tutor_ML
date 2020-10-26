c3ca163857f12d19eb066766d0f97c2d1fccfc1a
vyachkov <viachkov@hitmail.com>
2020-10-26 15:27:40 +0200

    pure network with several incomes

--- lesson_03.py
+++ lesson_03.py
@@ -1,5 +1,16 @@
 def main():
-    pass
+    def w_sum(a, b):
+        assert len(a) == len(b)
+        output = 0
+        for i in range(len(a)):
+            output += a[i] * b[i]
+        return output
+
+    weights = [0.1, 0.2, -1]
+
+    def neural_network(income, weights):
+        pred = w_sum(income, weights)
+        return pred
 
 
 

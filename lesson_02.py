5f4feb147082dc3c36bf1d7476448c0724a77918
vyachkov <viachkov@hitmail.com>
2020-10-26 09:19:07 +0200

    end_lesson_2

--- lesson_02.py
+++ lesson_02.py
@@ -1,21 +1,16 @@
 def main():
     weight = 0.5
-    alpha = 0.01    # speed of learning
+    goal_pred = 0.8
+    income = 2 
+    alpha = 0.1
 
-    def neural_network(income, weight):
-        pred = income * weight
-        return pred
+    for i in range(20):
+        prediction = income * weight
+        error = (prediction - goal_pred)**2
+        derivate = (prediction - goal_pred) * income
+        weight -= derivate * alpha
 
-    number_of_toes = [0.5]
-    win_or_lose_binary = [1]
-    income = number_of_toes[0]
-    goal_pred = win_or_lose_binary[0]
-
-    prediction = neural_network(income, weight)
-    error = (prediction - goal_pred)**2
-    delta = prediction - goal_pred
-    weight_delta = delta * income
-    weight -= weight_delta * alpha
+        print('Error: ' + str(error) + ' Prediction: ' + str(prediction))
 
 
 

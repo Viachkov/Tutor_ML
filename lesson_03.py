4c5a25ce8bb03523f0bc7998814b301f8b35a767
vyachkov <viachkov@hitmail.com>
2020-10-30 21:49:13 +0200

    last correction lesson_03

--- lesson_03.py
+++ lesson_03.py
@@ -24,6 +24,7 @@ def main():
         pred = vect_mat_mult(income, weights)
         return pred
     
+
     toes = [8.5, 9.5, 9.9, 9.0]
     wlrec = [0.65, 0.8, 0.8, 0.9]
     nfans = [1.2, 1.3, 0.5, 1.0]
@@ -50,6 +51,7 @@ def main():
     def other_prod(vec_a, vec_b):
         out = zero_matrix(len(a), len(b))
 
+
         for i in range(len(a)):
             for j in range(len(b)):
                 out[i][j] = a[i] * b[j]
@@ -61,7 +63,7 @@ def main():
 
     for i in range(len(goal_prediction)):
         error[i] = (prediction[i] - goal_prediction[i])**2
-        delta[i] = prediction[i] - goal_prediction[i]
+        delta = prediction[i] - goal_prediction[i]
 
     weight_deltas = other_prod(income, delta)
 

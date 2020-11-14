27954c74c6253b73ed57b6293394f6ee33996e4f
vyachkov <viachkov@hitmail.com>
2020-11-14 14:12:40 +0200

    Correct test

--- test.py
+++ test.py
@@ -10,6 +10,14 @@ def relu2deriv(output):
     # return 1 if output > 0 or 0
     return output > 0
 
+
+streetlights = np.array([[1, 0, 1],
+                        [0, 1, 1],
+                        [0, 0, 1],
+                        [1, 1, 1]])
+
+walk_vs_stop = np.array([1, 1, 0, 0]).T
+
 alpha = 0.2
 hidden_size = 4
 
@@ -20,7 +28,7 @@ for iteration in range(60):
     layer_2_error = 0
     for i in range(len(streetlights)):
         layer_0 = streetlights[i: i + 1]
-        layer_1  = relu(np.dot(layer_0, weieght_0_1))
+        layer_1  = relu(np.dot(layer_0, weight_0_1))
         layer_2 = np.dot(layer_1, weight_0_2)
 
         layer_2_error += np.sum((layer_2 - walk_vs_stop[i: i + 1])**2)
@@ -29,11 +37,11 @@ for iteration in range(60):
 
         """ строка вычисляет разность в слое layer_1 с учетом разности в слое layer_2,
         умножая layer_2-delta соответствующие веса weights_0_2"""
-        layer_1_delta = layer_2_delta.dot(weight_1_2.T) * relu2deriv(leyer_1)
+        layer_1_delta = layer_2_delta.dot(weight_0_2.T) * relu2deriv(layer_1)
         
-        weight_0_2 += alpha * layer_1.T.dot(layer_2_delta)
-        weight_0_1 += alpha * layer_0.T.dot(layer_1_delta)
+        weight_0_2 -= alpha * layer_1.T.dot(layer_2_delta)
+        weight_0_1 -= alpha * layer_0.T.dot(layer_1_delta)
 
-        if (iteration % 10 == 9):
-            print('Error: ' + str(layer_2_error))
-        
+    if (iteration % 10 == 9):
+        print('Error: ' + str(layer_2_error))
+    

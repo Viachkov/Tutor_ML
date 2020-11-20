22bbfefe0cdbb3f1d0743af1faba2b1b6599fd9b
vyachkov <viachkov@hitmail.com>
2020-11-20 09:54:40 +0200

    correct figure first test

--- lesson_05.py
+++ lesson_05.py
@@ -4,15 +4,15 @@ import sys
 import tensorflow as tf
 
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()a()
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-images, labels = (x_train[0:1000].reshape(1000, 28 * 28) / 255, y_train[0:1000])28) / 255, y_train[0:1000])
+images, labels = (x_train[0:1000].reshape(1000, 28 * 28) / 255, y_train[0:1000])
 one_hot_labels = np.zeros((len(labels), 10))
 for i, l in enumerate(labels):
     one_hot_labels[i][l] = 1
 labels = one_hot_labels
 
-test_images = x_test.reshape(len(x_test), 28 * 28) /2555
+test_images = x_test.reshape(len(x_test), 28 * 28) /255
 test_lagels = np.zeros((len(y_test), 10))
 for i, l in enumerate(y_test):
     test_lagels[i][l] = 1
@@ -20,10 +20,10 @@ for i, l in enumerate(y_test):
 np.random.seed(1)
 relu = lambda x: x > 0 * x
 relu2deriv = lambda output: output > 0
-alpha, iteration, hidden_size, pixels_per_image, num_labels = (0.005, 350, 40, 784, 10)labels = (0.005, 350, 40, 784, 10)
+alpha, iteration, hidden_size, pixels_per_image, num_labels = (0.005, 350, 40, 784, 10)
 
-weights_0_1 = 0.2 * np.random.random((pixels_per_image, hidden_size)) - 0.1e, hidden_size)) - 0.1
-weights_1_2 = 0.2 * np.random.random((hidden_size, num_labels)) - 0.1m_labels)) - 0.1
+weights_0_1 = 0.2 * np.random.random((pixels_per_image, hidden_size)) - 0.1
+weights_1_2 = 0.2 * np.random.random((hidden_size, num_labels)) - 0.1
 
 for j in range(iteration):
     error, correct_cnt = (0.0, 0)
@@ -32,15 +32,15 @@ for j in range(iteration):
         layer_1 = relu(np.dot(layer_0, weights_0_1))
         layer_2 = np.dot(layer_1, weights_1_2)
 
-        error += np.sum((labels[i:i + 1] - layer_2)**2)2)
-        correct_cnt += int(np.argmax(layer_2) == np.argmax(labels[i:i + 1]))rgmax(labels[i:i + 1]))
+        error += np.sum((labels[i:i + 1] - layer_2)**2)
+        correct_cnt += int(np.argmax(layer_2) == np.argmax(labels[i:i + 1]))
 
         layer_2_delta = (labels[i:i + 1] - layer_2)
-        layer_1_delta = layer_2_delta.dot(weights_1_2.T) * relu2deriv(layer_1).T) * relu2deriv(layer_1)
+        layer_1_delta = layer_2_delta.dot(weights_1_2.T) * relu2deriv(layer_1)
 
-        weights_1_2 += alpha * layer_1.T.dot(layer_2_delta)delta)
-        weights_0_1 += alpha * layer_0.T.dot(layer_1_delta)delta)
+        weights_1_2 += alpha * layer_1.T.dot(layer_2_delta)
+        weights_0_1 += alpha * layer_0.T.dot(layer_1_delta)
 
-        sys.stdout.write('\r' + 'I: ' + str(j) + ' Error: '\ror: ' \
-            + str(error/float(len(images)))[0:5] + ' Correct: '\Correct: ' \
+        sys.stdout.write('\r' + 'I: ' + str(j) + ' Error: '\
+            + str(error/float(len(images)))[0:5] + ' Correct: '\
             + str(correct_cnt / float(len(images))))
